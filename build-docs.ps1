e79b7fb8b124f33b97bf589c6a9524d06d7afaa7
Justin Colannino <justin.colannino@microsoft.com>
2019-11-07 21:31:45 -0500

    Update build-docs.ps1
    1.0 Release

--- build-docs.ps1
+++ build-docs.ps1
@@ -1,2 +1,4 @@
-pandoc .\C-UDA-0.1_annotated_discussion-draft.md -o .\C-UDA-0.1_annotated_discussion-draft.docx
-pandoc .\C-UDA-0.1_annotated_discussion-draft.md -o .\C-UDA-0.1_annotated_discussion-draft.pdf
+pandoc .\C-UDA-1.0.md -o .\C-UDA-1.0.docx
+pandoc .\C-UDA-1.0.md -o .\C-UDA-1.0.pdf
+pandoc .\C-UDA-1.0_annotated.md -o .\C-UDA-1.0_annotated.docx
+pandoc .\C-UDA-1.0_annotated.md -o .\C-UDA-1.0_annotated.pdf
